758088d32d84dde5922d66ff4eecbf513925944e
m.bompaire <m.bompaire@criteo.com>
2026-03-28 00:01:18 +0100

    Port tick.base_model and tick.linear_model

--- tick/preprocessing/__init__.py
+++ tick/preprocessing/__init__.py
@@ -1,11 +1,17 @@
 # License: BSD 3 clause
 
 from .features_binarizer import FeaturesBinarizer
-from .longitudinal_features_product import LongitudinalFeaturesProduct
-from .longitudinal_features_lagger import LongitudinalFeaturesLagger
-from .longitudinal_samples_filter import LongitudinalSamplesFilter
 from .utils import safe_array, check_censoring_consistency,\
     check_longitudinal_features_consistency
+
+try:
+    from .longitudinal_features_product import LongitudinalFeaturesProduct
+    from .longitudinal_features_lagger import LongitudinalFeaturesLagger
+    from .longitudinal_samples_filter import LongitudinalSamplesFilter
+except ImportError:
+    LongitudinalFeaturesProduct = None
+    LongitudinalFeaturesLagger = None
+    LongitudinalSamplesFilter = None
 
 __all__ = [
     "FeaturesBinarizer", "LongitudinalFeaturesProduct",
